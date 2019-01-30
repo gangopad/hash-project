db4c11d9fb7fa2f0062d3fcf787d4dab12c309d2
Joshua Zweig <jmz2135@columbia.edu>
2019-01-29 21:58:55 -0500

    Clean up working dir

--- README.md
+++
@@ -1,4 +0,0 @@
-# hash-project
-Work on novel methods for hash function entropy reduction.
-
-Anirban Gangopadhyay, Joshua Zweig
--- md5.py
+++ md5.py
@@ -57,25 +57,12 @@ def md5(message):
 
             #Gather hash data
             str_hash = str(chunk)
-            if (str_hash, d, a) not in adj_list:
-                adj_list[(str_hash, d, a)] = 1
+            from_state = a + b + c + d
+            to_state = d + new_b + b + c
+            if (str_hash, from_state, to_state) not in adj_list:
+                adj_list[(str_hash, from_state, to_state)] = 1
             else:
-                adj_list[(str_hash, d, a)] += 1
-
-            if (str_hash, new_b, b) not in adj_list:
-                adj_list[(str_hash, new_b, b)] = 1
-            else:
-                adj_list[(str_hash, new_b, b)] += 1
-
-            if (str_hash, b, c) not in adj_list:
-                adj_list[(str_hash, b, c)] = 1
-            else:
-                adj_list[(str_hash, b, c)] += 1
-
-            if (str_hash, c, d) not in adj_list:
-                adj_list[(str_hash, c, d)] = 1
-            else:
-                adj_list[(str_hash, c, d)] += 1
+                adj_list[(str_hash, from_state, to_state)] += 1
 
             a, b, c, d = d, new_b, b, c
 
@@ -94,14 +81,35 @@ def absoluteFilePaths(directory):
         for f in filenames:
             yield os.path.abspath(os.path.join(dirpath, f))
 
+def save_as_pickled_object(obj, filepath):
+    """
+    This is a defensive way to write pickle.write, allowing for very large files on all platforms
+    """
+    max_bytes = 2**31 - 1
+    bytes_out = pickle.dumps(obj)
+    n_bytes = sys.getsizeof(bytes_out)
+    with open(filepath, 'wb') as f_out:
+        for idx in range(0, n_bytes, max_bytes):
+            f_out.write(bytes_out[idx:idx+max_bytes])
+
 # Run 
 bin_dir = sys.argv[1]
-for filename in absoluteFilePaths(bin_dir):
+
+for i, filename in enumerate(absoluteFilePaths(bin_dir)):
+    print(filename)
     with open(filename, 'rb') as cur_file:
         bit_str = cur_file.read()
         #print(md5_to_hex(md5(bit_str)))
         md5(bit_str)
+    if i % 10000 == 0:
+        print(i)
+    if i == 100000 or i == 1000000:
+        save_as_pickled_object(adj_list, sys.argv[2] + 'md5states_' + str(i) + '.pickle')
+        exit(0)
+        #with open(sys.argv[2] + 'md5states_' + str(i) + '.pickle', 'wb') as out_file:
+            #out_file.write(pickle.dumps(adj_list))
 
-    with open("md5states.pickle", 'wb') as out_file:
-        out_file.write(pickle.dumps(adj_list))
+save_as_pickled_object(adj_list, sys.argv[2] + 'md5states_final.pickle')
 
+# with open(sys.argv[2] + 'md5states_final.pickle', 'wb') as out_file:
+#     out_file.write(pickle.dumps(adj_list))
