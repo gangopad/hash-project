dae226962bbff84117df7846e566a7090a27edae
Anirban Gangopadhyay <gangopad@gmail.com>
2019-02-11 22:26:39 -0500

    adding code for theorem 1

--- experiments/thm1.py
+++ experiments/thm1.py
@@ -1,5 +1,8 @@
 # Routine to read in DS from data.txt file 
 import numpy as np 
+from scipy.stats import entropy
+import sys
+from random import randint
 
 # @params filepath of structured data
 # @return X. X is a dictoinary key'd by x valued by a list of states 
@@ -24,38 +27,80 @@ def read_in_x(filepath, start, end):
 	return X
 
 
-def read_in_x_b0(filepath):
-	return read_in_x(0,15)
+def read_in_x_b(filepath, b):
+	b_start = b[0]
+	b_end = b[1]
+	return read_in_x(b_start, b_end)
 
-def read_in_x_b1(filepath):
-	return read_in_x(16,31)
 
-def read_in_x_b2(filepath):
-	return read_in_x(32,47)
+#computes the entropy given the list Z
+def computeEntropy(Z, base):
+	value,counts = np.unique(labels, return_counts=True)
+  	return entropy(counts, base=base)
 
-def read_in_x_b3(filepath):
-	return read_in_x(48,63)
 
 def theorem_1_routine(e, X):
-	return e #filler 
+	entropy = sys.maxsize
+	N = len(X)
+	Z = []
+
+	while entropy > e:
+		rand_index = randint(0, N)
+		inputs = X.keys()
+		x = inputs[rand_index]
+		z = X[x]
+		Z.extend(z)
+		entropy = computeEntropy(Z, 2)
 
 
-epsilon = np.arange(0, 1, step=.05)
-dataset_paths = ['data0.txt', 'data1.txt', 'data2.txt']
+	return entropy #filler 
 
-for ds_path in dataset_paths:
+
+
+
+"""
+Given a block, computes the number of examples 
+needed to get H(Z) < epsilon over datasets
+"""
+def computeBlock(b, epsilon):
+	dataset_paths = ['data0.txt', 'data1.txt', 'data2.txt']
+
+	for ds_path in dataset_paths:
+		res = computeDataset(ds_path, b, epsilon)
+		plot(res)
+
+"""
+Given a dataset, computes the number of examples 
+needed to get H(Z) < epsilon
+"""
+def computeDataset(ds_path, b, epsilon):
+	res = dict()
+	epsilon_res = dict()
+
 	for e in epsilon: 
-		X = read_in_x_b0(ds_path)
+		X = read_in_x_b(ds_path, b)
 		m = theorem_1_routine(e, X)
+		epsilon_res[e] = m
 
-		X = read_in_x_b1(ds_path)
-		m = theorem_1_routine(e, X)
+	res[ds_path] = epsilon_res
 
-		X = read_in_x_b2(ds_path)
-		m = theorem_1_routine(e, X)
+	return res
 
-		X = read_in_x_b3(ds_path)
-		m = theorem_1_routine(e, X)
+
+#generates a line plot over values of 
+def plot(res):
+	
+
+
+
+if __name__ == "__main__":
+	epsilon = np.arange(0, 1, step=.05)
+	blocks = [[0, 15], [16,31], [32,47], [48,63]]
+
+	for b in blocks:
+		computeBlock(b, epsilon)
+
+	
 
 
 
