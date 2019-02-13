1fb1af1efb2cc522b250ea92b83c78141ee05ad9
Joshua Zweig <jzweig@palantir.com>
2019-02-12 23:45:09 -0500

    BOOM AGAIN

--- experiments/thm2.py
+++ experiments/thm2.py
@@ -34,11 +34,11 @@ def read_in_x(filepath, state_ranges, n):
 
 	# Build data structure
 	pdfs_by_b = []
+	state_counts_by_b = []
 
 	for pair in state_ranges:
 		pdfs_by_b.append([{}, {}])
-
-	state_counts = {}
+		state_counts_by_b.append({})
 
 	# Select which N to keep for x init 
 	x_init_indices = random.sample(range(0, MAX_INDEX), n)
@@ -61,17 +61,17 @@ def read_in_x(filepath, state_ranges, n):
 
 				# Set x 
 				input_block = line[6:]
-				X.append(input_block)
 				offset = 0
 			else: 
+				X.append(input_block)
 				z_i = line
 				rnd = index_of_range(offset, state_ranges)
 				if rnd is not None:
 					# Add to state_counts list 
-					if z_i in state_counts:
-						state_counts[z_i] = state_counts[z_i] + 1
+					if z_i in state_counts_by_b[rnd]:
+						state_counts_by_b[rnd][z_i] = state_counts_by_b[rnd][z_i] + 1
 					else:
-						state_counts[z_i] = 1
+						state_counts_by_b[rnd][z_i] = 1
 
 					# If this is in x_init, add states 
 					if cur_in_range:
@@ -91,7 +91,7 @@ def read_in_x(filepath, state_ranges, n):
 
 			offset = offset + 1
 
-	return pdfs_by_b, state_counts, X, x_init
+	return pdfs_by_b, state_counts_by_b, X, x_init
 
 def index_of_range(i, state_ranges):
 	for n, sr in enumerate(state_ranges):
@@ -103,10 +103,7 @@ def post_process_pdfs(pdf, state_counts, X):
 	dic = {}
 	for key in pdf[1]:
 		value = pdf[1][key]
-		print("VALUE " + str(value))
-		print("DENOM " + str(X.count(key[1])))
 		dic[key] = value / float(X.count(key[1])) #x
-		#print(dic[key])
 
 	return dic
 
@@ -148,9 +145,8 @@ def getTopM(x_new, M):
 	for key, value in sorted(x_new.iteritems(), key=lambda (k,v): (v,k)):
 		count = count + 1
 		top_x.add(key)
-
-	return top_x
-
+		if count > M:
+			return top_x
 
 #returns the list of Xs with values for x_new and p(x) over the set of inputted states
 def getNewX(states_pdf, conditional_states_pdf, input_space, states):
@@ -160,15 +156,11 @@ def getNewX(states_pdf, conditional_states_pdf, input_space, states):
 		prob = 0.0
 		for z in states:
 			if (z, x) in conditional_states_pdf: 
-				# print(prob)
-				print((1.0/len(input_space)))
-				print(conditional_states_pdf[(z, x)])
-				print(states_pdf[z])
 				prob = prob + ((conditional_states_pdf[(z, x)] * (1.0/len(input_space))) / states_pdf[z])
 
 		X_new[x] = prob
-		print "For " + str(x) + " the probability is " + str(prob)
-		return X_new
+		#print "For " + str(x) + " the probability is " + str(prob)
+	return X_new
 
 
 if __name__ == "__main__":
@@ -176,14 +168,15 @@ if __name__ == "__main__":
 #	dataset_paths = ['data0.txt', 'data1.txt', 'data2.txt']
 	dataset_paths = ['data0_short.txt']
 	state_ranges = [[0, 15], [16,31], [32,47], [48,63]]
-	n = 10
+	n = 3
 
-	for M in np.arange(100, 10000, step=1000): #Alter later
+	#for M in np.arange(100, 10000, step=1000): #Alter later
+	for M in [1]:
 		for ds_path in dataset_paths:
 			pdfs_by_b, state_counts, X, X_init = read_in_x(ds_path, state_ranges, n)
-			for b in pdfs_by_b: 
-				z_given_x = post_process_pdfs(b, state_counts, X)
-				p_z = compute_p_z(state_counts)
+			for i, b in enumerate(pdfs_by_b): 
+				z_given_x = post_process_pdfs(b, state_counts[i], X)
+				p_z = compute_p_z(state_counts[i])
 				x_new = advAlg(M, p_z, z_given_x, X, X_init)
 				print "X new is " + str(x_new)
 				top_x = getTopM(x_new, M)
