c228568f17259b005affafa3a33d96062bc7160f
Joshua Zweig <jzweig@palantir.com>
2019-02-13 17:00:46 -0500

    working thm1

--- experiments/thm1.py
+++ experiments/thm1.py
@@ -27,20 +27,20 @@ def read_in_x(filepath, blocks):
 	print "Reading in X..."
 	with open(filepath, 'r') as infile:
 		for line in infile:
+			line = line.strip('\n')
 			if line[0:5] == 'block':
-				if input_block is not None:	
-					X[input_block] = states
-
-				states = []
 				input_block = line[6:]
 				offset = 0
 			else:
 				rnd = index_of_range(offset, blocks)
 				if rnd is not None:
-					new_list = X_all[rnd]
-					new_list.append(line)
-					X_all[rnd] = new_list
-        
+					if input_block in X_all[rnd]:
+						new_list = X_all[rnd][input_block]
+						new_list.append(line)
+						X_all[rnd][input_block] = new_list
+					else:
+						X_all[rnd][input_block] = [line]
+
 			offset = offset + 1
     
 	return X_all
@@ -58,14 +58,22 @@ def computeEntropy(Z, base):
 
 def theorem_1_routine(e, X):
 	print "Epsilon value: " + str(e)
-	entropy = sys.maxsize
+	entropy = 0
+	prev_entropy = 0
 	N = len(X)
 	Z = []
 	m = 0
 
-	while entropy > e:
+	z_actual = []
+	for k in X:
+		z_actual.extend(X[k])
+	kappa = computeEntropy(z_actual, 2)
+	print("Kappa: " + str(kappa))
+
+	while m < N:
+		prev_entropy = entropy
 		m = m + 1
-		rand_index = randint(0, N)
+		rand_index = randint(0, N - 1)
 		inputs = X.keys()
 		x = inputs[rand_index]
 		z = X[x]
@@ -74,28 +82,19 @@ def theorem_1_routine(e, X):
 
 		print "Entropy: " + str(entropy)
 
+		if entropy < e * kappa and entropy < prev_entropy:
+			return m
+
 	return m
 
 """
 Given a block, computes the number of examples 
 needed to get H(Z) < epsilon over datasets
 """
-def computeBlock(b, epsilon, ds_path):
-	res = computeDataset(ds_path, b, epsilon)
-	plot(res)
+def computeBlock(b, i, epsilon, ds_path, X):
+	res = computeDataset(ds_path, b, epsilon, X)
+	plot(res, epsilon, i)
 
-def computeEntropy(X):
-	states_dict = {} 
-	states_list = []
-	for s in X:
-		states_list.extend(X[s])
-
-	for s in states_list:
-		states_dict[s] = states_list.count(s)
-
-	for k, v in hex_strings.items():
-  		p_x = getcontext().divide(Decimal(v), Decimal(num_uniq_strs))
-  		return getcontext().add(entropy, getcontext().multiply(p_x, p_x.ln()))
 
 """
 Given a dataset, computes the number of examples 
@@ -108,7 +107,6 @@ def computeDataset(ds_path, b, epsilon, X):
 	for e in epsilon: 
 		print "Tuple: " + str(b) + " epsilon: " + str(e)
 		#X = read_in_x_b(ds_path, b)
-		print("Real Entropy: " + str(computeEntropy(X)))
 		m = theorem_1_routine(e, X)
 		epsilon_res.append(m)
 
@@ -119,22 +117,22 @@ def computeDataset(ds_path, b, epsilon, X):
 	return res
 
 #generates a line plot over values of epsilon 
-def plot(res, epsilon):
-	df=pd.DataFrame({'x': epsilon, 'y1': res['data0.txt'], 'y2': res['data1.txt'], 'y3': res['data2.txt'] })
+def plot(res, epsilon, i):
+	df=pd.DataFrame({'x': epsilon, 'y1': res['data0_short.txt'], 'y2': res['data0_short.txt'], 'y3': res['data0_short.txt'] })
 	fout = open("thm1.pdf", "wb")
 	pickle.dump(df, fout)
 
 
 	# multiple line plot
-	plt.plot( 'x', 'y1', data=df, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4, label="data0.txt")
-	plt.plot( 'x', 'y2', data=df, marker='', color='olive', linewidth=2, label="data1.txt")
-	plt.plot( 'x', 'y3', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="data2.txt")
+	plt.plot( 'x', 'y1', data=df, marker='o', color='blue', linewidth=2, label="data0.txt")
+	plt.plot( 'x', 'y2', data=df, marker='x', color='red', linewidth=2, label="data1.txt")
+	plt.plot( 'x', 'y3', data=df, marker='d', color='black', linewidth=2, linestyle='dashed', label="data2.txt")
 	plt.legend()
 	plt.xlabel("Epsilon")
 	plt.ylabel("M")
-	plt.title("Number of examples until H(Z) < epsilon")
-	f = plt.figure()
-	f.savefig("t1_plot.png")
+	plt.title("Number of examples until H(Z) < epsilon for B " + str(i))
+	plt.savefig("b_" + str(i) + ".png")
+	plt.clf()
 
 if __name__ == "__main__":
 	epsilon = np.arange(0, 1, step=.05)
@@ -145,4 +143,4 @@ if __name__ == "__main__":
 		X_all = read_in_x(ds_path, blocks)
 
 		for i, b in enumerate(blocks):
-			computeBlock(b, epsilon, X_all[i], ds_path)
+			computeBlock(b, i, epsilon, ds_path, X_all[i])
